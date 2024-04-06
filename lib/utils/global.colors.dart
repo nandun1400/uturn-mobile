ea61cf329c9fe782dcc2b1da1a38b88ab269b0a1
Akash Niyas <induranga200103@gmail.com>
2024-04-07 00:04:15 +0530

    10898740
    Handurukande Mudiyanselage Haritha Induranga
    
    Co-Authored-By: nandun1400 <160683599+nandun1400@users.noreply.github.com>

---
+++ lib/utils/global.colors.dart
@@ -0,0 +1,7 @@
+import 'package:hexcolor/hexcolor.dart';
+
+class GlobalColors {
+  //// 1E319D main color
+  static HexColor mainColor = HexColor('#1E319D');
+  static HexColor textColor = HexColor('#4F4F4F');
+}
